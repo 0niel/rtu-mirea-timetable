b28aa1af769d0f7706b013c9e89bd77fe3b991b8
Oniel <51058739+0niel@users.noreply.github.com>
2022-10-25 00:07:58 +0300

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -10,9 +10,18 @@ app = FastAPI(
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
+    
+    # Allow all origins.
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
