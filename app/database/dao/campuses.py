45e573255d703956bb6b1bd4e16c0b75777f0859
Dragonprod <artyom2002-98@mail.ru>
2022-12-19 15:44:26 +0300

    fix: select

--- app/database/dao/campuses.py
+++ app/database/dao/campuses.py
@@ -14,7 +14,7 @@ class CampusesDAO:
 
     async def get_campuses(self) -> list[models.Campus]:
         """Получение всех кампусов"""
-        query = select(select(tables.ScheduleCampus))
+        query = select(tables.ScheduleCampus)
         campuses = (await self._session.execute(query)).scalars().all()
         result = [models.Campus.from_orm(campus) for campus in campuses]
         return result
