15819c65b9498abba4e793c1540e8d88c8b2913b
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-03-12 17:36:25 +0300

    feature: Parse Schedule in ThreadPoolExecutor

--- app/parser/schedule.py
+++ app/parser/schedule.py
@@ -1,10 +1,11 @@
 import datetime
 import json
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Generator
 
 from loguru import logger
-from rtu_schedule_parser import ExcelScheduleParser, LessonEmpty, LessonsSchedule
+from rtu_schedule_parser import ExamsSchedule, ExcelScheduleParser, LessonEmpty, LessonsSchedule
 from rtu_schedule_parser.constants import Degree, Institute, ScheduleType
 from rtu_schedule_parser.downloader import ScheduleDownloader
 from rtu_schedule_parser.utils import academic_calendar
@@ -24,11 +25,27 @@ from app.services.db import (
 
 class ScheduleParsingService:
     @classmethod
-    async def parse_schedule(cls, db: AsyncSession) -> None:  # sourcery skip: low-code-quality
+    def _parse(cls) -> Generator[list[LessonsSchedule | ExamsSchedule], None, None]:
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            # создаем список задач по загрузке и парсингу документов
+            tasks = []
+            for doc in cls._get_documents():
+                task = executor.submit(cls._parse_document, doc)
+                tasks.append(task)
+
+            for future in as_completed(tasks):
+                if schedules := future.result():  # type: list[LessonsSchedule | ExamsSchedule]
+                    groups = {schedule.group for schedule in schedules}
+                    logger.info(f"Получено расписание документа. Группы: {groups}")
+                    yield schedules
+
+    @classmethod
+    async def parse_schedule(cls, db: AsyncSession) -> None:
         """Парсинг расписания используя пакет rtu_schedule_parser"""
 
         for schedules in cls._parse():
             for schedule in schedules:
+                logger.info(f"Сохраняем расписание группы {schedule.group} в БД")
                 period = await PeriodDBService.get_period_by_params(
                     db,
                     year_start=schedule.period.year_start,
@@ -168,12 +185,11 @@ class ScheduleParsingService:
                         )
 
     @classmethod
-    def _parse(cls) -> Generator[list[LessonsSchedule], None, None]:
-        """Parse parser from excel file"""
+    def _get_documents(cls) -> list:
+        """Get documents for specified institute and degree"""
         docs_dir = os.path.dirname(os.path.abspath(__file__))
         docs_dir = os.path.join(docs_dir, "docs")
 
-        # Initialize downloader with default directory to save files
         downloader = ScheduleDownloader(base_file_dir=docs_dir)
 
         if os.path.exists(docs_dir):
@@ -186,14 +202,13 @@ class ScheduleParsingService:
         else:
             os.mkdir(docs_dir)
 
-        # Get documents for specified institute and degree
         all_docs = downloader.get_documents(
             specific_schedule_types={ScheduleType.SEMESTER},
             specific_degrees={Degree.BACHELOR, Degree.MASTER, Degree.PHD},
             specific_institutes={institute for institute in Institute if institute != Institute.COLLEGE},
         )
 
-        logger.info(f"Found {len(all_docs)} documents to parse")
+        logger.info(f"Найдено {len(all_docs)} документов для парсинга")
 
         # Download only if they are not downloaded yet.
         downloaded = downloader.download_all(all_docs)
@@ -201,24 +216,24 @@ class ScheduleParsingService:
         # сначала документы с Degree.PHD, потом Degree.MASTER, потом Degree.BACHELOR (то есть по убыванию)
         downloaded = sorted(downloaded, key=lambda x: x[0].degree, reverse=True)
 
-        logger.info(f"Downloaded {len(downloaded)} files")
+        logger.info(f"Скачано {len(downloaded)} файлов")
 
-        for doc in downloaded:
-            print(f"Processing document: {doc}")
-            try:
-                parser = ExcelScheduleParser(doc[1], doc[0].period, doc[0].institute, doc[0].degree)
+        documents = [(doc[1], doc[0].period, doc[0].institute, doc[0].degree) for doc in downloaded]
+        documents += cls._get_documents_by_json(docs_dir)
 
-                yield parser.parse(force=True).get_schedule()
-            except Exception:
-                logger.error("Parsing exception from pkg")
-
-        docs_dir = os.path.dirname(os.path.abspath(__file__))
-        docs_dir = os.path.join(docs_dir, "..", "..", "docs")
-
-        yield from cls._parse_by_json(docs_dir)
+        return documents
 
     @classmethod
-    def _parse_by_json(cls, docs_dir: str) -> Generator[list[LessonsSchedule], None, None]:
+    def _parse_document(cls, doc) -> list[LessonsSchedule | ExamsSchedule]:
+        logger.info(f"Обработка документа: {doc}")
+        try:
+            parser = ExcelScheduleParser(doc[0], doc[1], doc[2], doc[3])
+            return parser.parse(force=True).get_schedule()
+        except Exception:
+            logger.error(f"Парсинг завершился с ошибкой ({doc})")
+
+    @classmethod
+    def _get_documents_by_json(cls, docs_dir: str) -> list:
         # Формат json:
         # [
         #     {
@@ -228,33 +243,31 @@ class ScheduleParsingService:
         #         "degree": 4
         #     }
         # ]
-
         try:
             with open(os.path.join(docs_dir, "files.json"), "r") as f:
                 files = json.load(f)
 
+                documents = []
+
                 for file in files:
                     file_path = os.path.join(docs_dir, file["file"])
-                    logger.info(f"Parsing {file['file']} from `files.json`")
+                    logger.info(f"Файл {file['file']} добавлен на парсинг из `files.json`")
 
                     if not os.path.exists(file_path):
-                        logger.error(f"File {file['file']} not found. See `files.json` for more info. Skipping...")
+                        logger.error(f"Файл {file['file']} не найден. См. `files.json`. Пропускаем...")
                         continue
 
-                    try:
-                        now_date = datetime.datetime.now()
-                        parser = ExcelScheduleParser(
+                    documents.append(
+                        (
                             file_path,
-                            academic_calendar.get_period(now_date),
+                            academic_calendar.get_period(datetime.datetime.now()),
                             Institute.get_by_short_name(file["institute"]),
                             Degree(file["degree"]),
                         )
+                    )
 
-                        yield parser.parse(force=True).get_schedule()
-                    except Exception as e:
-                        logger.error(f"Error while parsing {file['file']}: {e}")
-                        continue
+                return documents
 
         except FileNotFoundError:
-            logger.error("File `files.json` not found. Skipping...")
-            return
+            logger.info("`files.json` не найден. Пропускаем...")
+            return []
